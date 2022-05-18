bb6f7b3f62ee9152a55dbe5ed044c34971c3afc6
hojaelee <dev.hojaelee@gmail.com>
2022-05-18 21:34:20 +0900

    cpu peak usage - excel 연동 전

--- gather_metric.ps1
+++ gather_metric.ps1
@@ -13,23 +13,29 @@ $excel_path = "/Users/hojaelee/Desktop/powershell/gather_metric.xlsx"
 $max = 0
 
 $raw_data = az monitor metrics list --resource "/subscriptions/ca9621d4-bfba-4a66-8f48-c4c7eaa0a21f/resourceGroups/HJ/providers/Microsoft.Compute/virtualMachines/vm-test" `
-    --metric "Percentage CPU" --interval 1m --offset 1d --aggregation Average | grep average
-
-#$raw_data.replace('"average": ', "")
-$raw_data = $raw_data -replace ('"average": ', "") -split "," -replace ('              ', '')#파싱을 위해서 빈칸 제거
+    --metric "Percentage CPU" --interval 1m --offset 30d --aggregation Average | grep average
+$raw_data = $raw_data -replace ('"average": ', "") -split "," -replace ('              ', '') #파싱을 위해서 빈칸 제거
 
 for ($cnt = 0; $cnt -lt ($raw_data.length - 1); $cnt += 1) {
-    if ('null' -eq $raw_data[$cnt]) {
+    if ('null' -like $raw_data[$cnt]) {
+        $raw_data.set($cnt, '')
+    }
+}
+$raw_data | Measure-Object -AllStats
+$total_stats = ($raw_data | Measure-Object -AllStats).maximum
+echo $total_stats
+
+
+<# Max를 직접 구현하기
+for ($cnt = 0; $cnt -lt ($raw_data.length - 1); $cnt += 1) {
+    if ('null' -like $raw_data[$cnt]) {
         $raw_data.set($cnt, 0)
     }
-}
-
-for ($cnt = 0; $cnt -lt ($raw_data.length - 1); $cnt += 1) {
-    if ($raw_data[$cnt] -gt $max) {
-        $max = $raw_data[$cnt]
+    #ADD-Content /Users/hojaelee/Desktop/powershell/type0.txt $raw_data[$cnt] #요건 txt append 함수
+    if ([double]$raw_data[$cnt] -gt [double]$max) {
+    $max = $raw_data[$cnt]
     }
 }
-
 Write-Output $max
+#>
 
-#echo $raw_data.replace('`n', '')
